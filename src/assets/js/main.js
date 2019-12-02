ee353a3747d36bfd4446a63744be8db1a6c31b2b
GD Kimaiyo <gdkimaiyo@gmail.com>
2019-12-02 20:18:34 +0300

    Check/Mark a task as complete

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,67 +1,108 @@
 // Const Variables
 const form = document.querySelector('form');
 const ol = document.querySelector('ol');
-const input = document.getElementById('todoItem');
+const input = document.querySelector('#todoItem');
 // Other Variables
-let newTodos;
+let allTodos;
 
 if (localStorage.getItem("todos")) {
-  newTodos = JSON.parse(localStorage.getItem("todos"));
+  allTodos = JSON.parse(localStorage.getItem("todos"));
 } else {
-  newTodos = [];
+  allTodos = [];
 }
 
 // assign allTodos
-localStorage.setItem("todos",JSON.stringify(newTodos));
-const allTodos = JSON.parse(localStorage.getItem("todos"));
+localStorage.setItem("todos",JSON.stringify(allTodos));
 
-form.addEventListener('submit', function addTodo(e) {
+// localStorage.clear();
+
+form.addEventListener('submit', (e) => {
   e.preventDefault();
 
-  // TODO:
-  if (input.value.length > 0) {
-    if (typeof(Storage) !== "undefined") {
-      const lastId = allTodos.length > 0 ? allTodos.length - 1 : 0;
-      const id = lastId + 1;
-      const completed = false;
-      const task = input.value;
-
-      const newTask = {id, task, completed};
-      newTodos.push(newTask);
-      console.log(newTask);
-
-      localStorage.setItem("todos",JSON.stringify(newTodos));
-      todoGenerator(newTask.task);
-
+  if (typeof(Storage) !== "undefined") {
+    const inputTxt = input.value.trim();
+    if (inputTxt !== '') {
+      addTodos(inputTxt);
+      // Reset and focus input
       input.value = "";
-    } else {
-      // No web storage Support.
-      console.log("No web storage Support!");
+      input.focus();
     }
+  } else {
+    // No web storage Support.
+    console.log("No Web Storage Support!");
   }
-
 });
 
 // Display all existing information
 console.log(allTodos);
 allTodos.forEach(item => {
-  todoGenerator(item.task);
+  todoGenerator(item.task,item.id);
 });
 
-const myli = document.querySelectorAll('li');
-for (var i=0; i<myli.length; i++) {
-  if (i > 0) {
-    myli[i].addEventListener('click',function(e) {
-      e.preventDefault();
-    });
+// Mark Todo task as complete
+const list = document.querySelector('.todo-list');
+list.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  if (e.target.classList.contains('to-check')) {
+    const todoKey = e.target.parentElement.dataset.key;
+    taskCompleted(todoKey);
   }
+});
+
+
+// Functions
+// Adding a Todo
+function addTodos(text) {
+  const newTodo = {
+    id: allTodos.length > 0 ? allTodos[allTodos.length - 1].id + 1 : 1,
+    task: text,
+    completed: false
+  };
+  allTodos.push(newTodo);
+  console.log(newTodo);
+
+  localStorage.setItem("todos",JSON.stringify(allTodos));
+  todoGenerator(newTodo.task, newTodo.id);
 }
 
-
-
-
-function todoGenerator(todo) {
+// Todo Generator
+function todoGenerator(todo,id) {
   const li = document.createElement('li');
-  li.textContent = todo;
+  li.setAttribute('data-key',id);
+
+  // checkbox
+  const checkbox = document.createElement('input');
+  checkbox.type = 'checkbox';
+  checkbox.id = id;
+  const label = document.createElement('label');
+  label.setAttribute('for',id);
+  label.classList.add('check', 'to-check');
+  // Span to hold Todo task
+  const span = document.createElement('span');
+  span.textContent = todo;
+  // Delete button
+  const delBtn = document.createElement('button');
+  delBtn.classList.add('danger-btn', 'to-delete');
+  delBtn.textContent = 'X';
+
+  li.appendChild(delBtn);
+  li.appendChild(checkbox);
+  li.appendChild(label);
+  li.appendChild(span);
+
   ol.appendChild(li);
+}
+
+// Task completed method
+function taskCompleted(key) {
+  const indx = allTodos.findIndex(item => item.id === Number(key));
+  allTodos[indx].completed = !allTodos[indx].completed;
+
+  const item = document.querySelector(`[data-key='${key}']`);
+  if (allTodos[indx].completed) {
+    item.classList.add('done');
+  } else {
+    item.classList.remove('done');
+  }
 }
